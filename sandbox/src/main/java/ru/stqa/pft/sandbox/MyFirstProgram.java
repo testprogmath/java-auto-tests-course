580a2f51abfef8a7089ba5066606e6b408a5b0eb
Anna Khvorostyanova <testprogmath@gmail.com>
2018-07-15 16:22:27 +0300

    Добавлено несколько функций (hello, area для квадрата и прямоугольника)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,25 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " равно " + area(len));
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+    }
 
+    private static void hello(String sb) {
+        System.out.println("Hello " + sb + "!");
+    }
+
+    private static double area(double l) {
+        return l * l;
+    }
+
+    private static double area(double a, double b) {
+        return a * b;
+    }
 }
