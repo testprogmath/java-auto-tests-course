76f0502ee13ad0b32f00864a622435041b56b09a
Anna Khvorostyanova <testprogmath@gmail.com>
2018-08-04 18:21:51 +0300

    Рефакторинг теста создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -4,16 +4,11 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class ContactCreationTests {
     FirefoxDriver wd;
@@ -23,25 +18,25 @@ public class ContactCreationTests {
         //указываем, где находится нужная версия FF для работы драйвера
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:\\Program Files\\Mozilla FirefoxESR52/firefox.exe"));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String user, String password) {
         wd.get("http://localhost/addressbook/index.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(user);
         wd.findElement(By.cssSelector("label")).click();
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
     public void ContactCreationTests() {
         initNewContact(By.linkText("add new"));
-        fillContactForm();
+        fillContactForm(new ContactRequiredData("Anna", "Vasileva", "a.vasileva@gmail.com", "1995", "+79992130923"));
         submitContactCreation("//div[@id='content']/form/input[21]");
         //через 10-15 секунд сервер переведет на форму просмотра контактов
     }
@@ -50,28 +45,30 @@ public class ContactCreationTests {
         wd.findElement(By.xpath(s)).click();
     }
 
-    private void fillContactForm() {
+    private void fillContactForm(ContactRequiredData contactRequiredData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Anna");
+        wd.findElement(By.name("firstname")).sendKeys(contactRequiredData.getFirstName());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Vasileva");
+        wd.findElement(By.name("lastname")).sendKeys(contactRequiredData.getLastName());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("a.vasileva@gmail.com");
+        wd.findElement(By.name("email")).sendKeys(contactRequiredData.getEmail());
         if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[20]")).isSelected()) {
+            //выбираем день из выпадающего списка для дня рождения
             wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[20]")).click();
         }
         if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[4]")).isSelected()) {
+            //выбираем месяц из выпадающего списка для месяца рождения
             wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[4]")).click();
         }
         wd.findElement(By.name("byear")).click();
         wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys("1995");
+        wd.findElement(By.name("byear")).sendKeys(contactRequiredData.getBirthYear());
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("+79992130923");
+        wd.findElement(By.name("mobile")).sendKeys(contactRequiredData.getMobilePhone());
     }
 
     private void initNewContact(By add_new) {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactRequiredData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactRequiredData {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String birthYear;
+    private final String mobilePhone;
+
+    public ContactRequiredData(String firstName, String lastName, String email, String birthYear, String mobilePhone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthYear = birthYear;
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+}
