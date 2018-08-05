de53a1a8aea974daaaae7a24fde80d09632f18a5
Anna Khvorostyanova <testprogmath@gmail.com>
2018-08-05 15:19:52 +0300

    Настройка запуска автотестов в разных браузерах (FF, IE и Chrome)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,22 +1,39 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.model.ContactRequiredData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager  {
-    FirefoxDriver wd;
+    private String browser;
+    WebDriver wd;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
 
     public void init() {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:\\Program Files\\Mozilla FirefoxESR52/firefox.exe"));
+        if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:\\Program Files\\Mozilla FirefoxESR52/firefox.exe"));
+        }
+        else if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        }
+        else if (browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
         contactHelper = new ContactHelper(wd);
