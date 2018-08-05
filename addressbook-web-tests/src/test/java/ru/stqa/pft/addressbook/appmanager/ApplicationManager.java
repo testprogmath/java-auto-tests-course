41555cadce6f6a45b300fc5998fb3bf373d3721c
Anna Khvorostyanova <testprogmath@gmail.com>
2018-08-05 12:59:14 +0300

    Создан класс вспомогательный ContactHelper, перенесен класс для данных контактов в пакет model

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,21 +3,23 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import ru.stqa.pft.addressbook.ContactRequiredData;
+import ru.stqa.pft.addressbook.model.ContactRequiredData;
 
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager {
+public class ApplicationManager  {
     FirefoxDriver wd;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private ContactHelper contactHelper;
 
 
     public void init() {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:\\Program Files\\Mozilla FirefoxESR52/firefox.exe"));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
+        contactHelper = new ContactHelper(wd);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
@@ -29,46 +31,17 @@ public class ApplicationManager {
         wd.quit();
     }
 
-    public void submitContactCreation(String s) {
-
-        wd.findElement(By.xpath(s)).click();
-    }
-
-    public void fillContactForm(ContactRequiredData contactRequiredData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactRequiredData.getFirstName());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactRequiredData.getLastName());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactRequiredData.getEmail());
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[20]")).isSelected()) {
-            //выбираем день из выпадающего списка для дня рождения
-            wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[20]")).click();
-        }
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[4]")).isSelected()) {
-            //выбираем месяц из выпадающего списка для месяца рождения
-            wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[4]")).click();
-        }
-        wd.findElement(By.name("byear")).click();
-        wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys(contactRequiredData.getBirthYear());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(contactRequiredData.getMobilePhone());
-    }
-
-    public void initNewContact(By add_new) {
-        wd.findElement(add_new).click();
+    public ContactHelper getContactHelper() {
+        return contactHelper;
     }
 
     public GroupHelper getGroupHelper() {
+
         return groupHelper;
     }
 
     public NavigationHelper getNavigationHelper() {
+
         return navigationHelper;
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,16 +3,16 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 
 import org.openqa.selenium.*;
-import ru.stqa.pft.addressbook.ContactRequiredData;
+import ru.stqa.pft.addressbook.model.ContactRequiredData;
 
 public class ContactCreationTests extends TestBase{
 
 
     @Test
     public void testContactCreation() {
-        app.initNewContact(By.linkText("add new"));
-        app.fillContactForm(new ContactRequiredData("Anna", "Vasileva", "a.vasileva@gmail.com", "1995", "+79992130923"));
-        app.submitContactCreation("//div[@id='content']/form/input[21]");
+        app.getContactHelper().initNewContact(By.linkText("add new"));
+        app.getContactHelper().fillContactForm(new ContactRequiredData("Anna", "Khvorostyanova", "a.vasileva@gmail.com", "1995", "+79992130923"));
+        app.getContactHelper().submitContactCreation("//div[@id='content']/form/input[21]");
         //через 10-15 секунд сервер переведет на форму просмотра контактов
     }
 
