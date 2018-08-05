1f377180804ff77e532769160f2e174ef4de9ff4
Anna Khvorostyanova <testprogmath@gmail.com>
2018-08-05 13:36:08 +0300

    Создан тест изменения контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -39,4 +39,16 @@ public class ContactHelper extends HelperBase {
     public void returnToContactsPage() {
         click(By.linkText("home page"));
     }
+
+    public void goToContactsPage() {
+        click(By.linkText("home"));
+    }
+
+    public void initContactModification() {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+
+    public void submitContactModification() {
+        click(By.xpath("//div[@id='content']/form[1]/input[22]"));
+    }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactRequiredData;
+
+public class ContactModificationTests extends TestBase {
+    @Test
+    public void testContactModification() {
+        app.getContactHelper().goToContactsPage();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactRequiredData("ChangedAnna", "ChangedKhvorostyanova", "Changed_a.vasileva@gmail.com", "1995", "+79992130900"));
+        app.getContactHelper().submitContactModification();
+        app.getContactHelper().returnToContactsPage();
+    }
+}
