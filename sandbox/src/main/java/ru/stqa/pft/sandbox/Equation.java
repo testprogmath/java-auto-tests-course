1a4cbe3ad4f60cb9193bfdc1ca7b87e4d65d3c3d
Anna Khvorostyanova <testprogmath@gmail.com>
2018-08-05 14:53:57 +0300

    Решение уравнений+тесты

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.sandbox;
+
+public class Equation {
+    private double a;
+    private double b;
+    private double c;
+
+    private int n; //количество корней уравнения
+
+    public Equation(double a, double b, double c) {
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        double d = b * b - 4 * a * c; //дискриминант
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                }
+                else {
+                    n = 1;
+                }
+            }
+            else {
+                n = 1;
+            }
+        }
+        else {
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
+                n = 1;
+            } else {
+                n = 0;
+            }
+        }
+    }
+        public int rootNumber() {
+            return n;
+        }
+    }
