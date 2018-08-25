d154c84506d6ebee49870d8e4adeafed150ebdfc
Anna Khvorostyanova <testprogmath@gmail.com>
2018-08-25 21:16:19 +0300

    Удалены лишние методы в ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactRequiredData;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -66,9 +65,6 @@ public class ContactHelper extends HelperBase {
     }
 
 
-    public void select(int index) {
-        wd.findElements(By.name("selected[]")).get(index).click();
-    }
 
     public void submitDeletion() {
         click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
@@ -81,28 +77,6 @@ public class ContactHelper extends HelperBase {
         returnToContactsPage();
     }
 
-    public boolean isThereAnyContact() {
-        return isElementPresent(By.name("entry"));
-    }
-
-    public List<ContactRequiredData> list() {
-        List<ContactRequiredData> contacts = new ArrayList<ContactRequiredData>();
-        List<WebElement> elements = wd.findElements(By.name("entry"));
-        for (WebElement element: elements){
-            /* создаем список, элементы находятся в следующем порядке: чек-бокс(0), Фамилия(1), Имя(2), адрес(3), e-mail(4), телефон(5), прочие картинки */
-            List<WebElement> td = element.findElements(By.tagName("td"));
-            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            String lastName = td.get(1).getText();
-            String name = td.get(2).getText();
-            String address = td.get(3).getText();
-            String email = td.get(4).getText();
-            String phone = td.get(5).getText();
-            ContactRequiredData contact = new ContactRequiredData()
-                    .withId(id).withFirstName(name).withLastName(lastName).withEmail(email).withMobilePhone(phone);
-            contacts.add(contact);
-        }
-        return contacts;
-    }
     public Set<ContactRequiredData> all() {
         Set<ContactRequiredData> contacts = new HashSet<>();
         List<WebElement> elements = wd.findElements(By.name("entry"));
