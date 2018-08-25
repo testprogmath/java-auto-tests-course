e2231057d0af32c6f2ee28856e63fac200ecb33f
Anna Khvorostyanova <testprogmath@gmail.com>
2018-08-25 18:53:33 +0300

    Вернула строчку с ассертом для теста создания групп (удалена по ошибке)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationsTests.java
@@ -24,5 +24,6 @@ public class GroupCreationsTests extends TestBase{
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
         after.sort(byId);
+        Assert.assertEquals(before, after);
     }
     }
