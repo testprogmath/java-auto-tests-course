e1c6fb3af306b194ddff398e1098ea85d8194259
Anna Khvorostyanova <testprogmath@gmail.com>
2018-09-10 08:43:39 +0300

    Добавлен ftpHelper

--- mantis/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
   private String browser;
   private DbHelper DbHelper;
   private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -69,5 +70,11 @@ public class ApplicationManager {
       registrationHelper = new RegistrationHelper(this);
     }
     return registrationHelper;
+  }
+  public FtpHelper ftp() {
+    if (ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
   }
   }
