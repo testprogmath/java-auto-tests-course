a4226bf7af8b209cc01ea4c7a0f6ff01b20bb8b8
Anna Khvorostyanova <testprogmath@gmail.com>
2018-10-28 14:18:44 +0300

    Добавлены предусловия в ContactDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactRequiredData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,8 +15,19 @@ public class ContactDeletionTests extends TestBase {
     public void ensurePreconditions() {
         app.goTo().homePage();
         if (app.db().contacts().size()==0) {
+            if (app.db().groups().size() == 0) {
+                app.goTo().groupPage();
+                app.group().create(new GroupData().withName("name1").withHeader("header1").withFooter("footer1"));
+            }
+            Groups groups = app.db().groups();
             ContactRequiredData contact = new ContactRequiredData()
-                    .withFirstName("Anna").withLastName("Khvorostyanova").withEmail("a.vasileva@gmail.com").withBirthYear("1995").withMobilePhone("+79992130923");
+                    .withFirstName("Anna")
+                    .withLastName("Khvorostyanova")
+                    .withEmail("a.vasileva@gmail.com")
+                    .withBirthYear("1995")
+                    .withMobilePhone("+79992130923")
+                    .inGroup(groups.iterator().next());
+            app.goTo().homePage();
         }
     }
 
